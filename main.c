19e1089567298355d67bb746c681b6ed7ae3ca70
roadou <thewwworld@gmail.com>
2025-11-28 21:56:26 +0100

    Initial Commit

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <string.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+char caesar_encrypt(char c, int key) {
+
+    if(c >= 'a' && c <= 'z') {
+        return ((c - 'a' + key) % 26) + 'a';
+    }
+    else if(c >= 'A' && c <= 'Z') {
+        return ((c - 'A' + key) % 26) + 'A';
+    }
+    else {
+        return c;
+    }
+}
+
+char caesar_decrypt(char c, int key) {
+    
+    key = (26 - key % 26) % 26;
+    if(c >= 'a' && c <= 'z') {
+        return ((c - 'a' + key) % 26) + 'a';
+    }
+    else if(c >= 'A' && c <= 'Z') {
+        return ((c - 'A' + key) % 26) + 'A';
+    }
+    else {
+        return c;
+    }
+}
+
+int main(int argc, const char* argv[]) {
+    char* endptr;
+
+    if(argc < 4) {
+        printf("usage: %s <encrypt/decrypt> <key> <value>\n", argv[0]);
+        return 1;
+    }
+    
+    int key = strtoimax(argv[2], &endptr, 10);
+    char* processed_string = malloc(strlen(argv[3]));
+
+    if(strcmp(argv[1], "--decrypt") == 0) {
+        for(int i = 0; i < strlen(argv[3]); i++)
+        {
+            processed_string[i] = caesar_decrypt(argv[3][i], key);
+        }
+        printf("Encrypted string is %s\n", processed_string);
+    }
+    else if(strcmp(argv[1], "--encrypt") == 0) {
+        for(int i = 0; i < strlen(argv[3]); i++)
+        {
+            processed_string[i] = caesar_encrypt(argv[3][i], key);
+        }
+        printf("Encrypted string is %s\n", processed_string);
+    }
+
+    free(processed_string);
+}
